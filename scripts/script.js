e36ffdd86c8854c65b5f8327b7a29ba47d24d3db
Harry Zhou <harryzh@students.cs.ubc.ca>
2023-03-11 15:35:09 -0800

    Created courses objects in javascript and stored in courselist array

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+const acit1620 = {
+    code: 'ACIT 1620',
+    name: 'Web Fundamental Technologies'
+}
+
+const acit1515 = {
+    code: 'ACIT 1515',
+    name: 'Scripting in IT'
+}
+
+const acit1630 = {
+    code: 'ACIT 1630',
+    name: 'Database Systems'
+}
+
+const acit1420 = {
+    code: 'ACIT 1420',
+    name: 'Intro to Systems Admin'
+}
+
+const orgb1100 = {
+    code: 'ORGB 1100',
+    name: 'Organizational Behaviour'
+}
+
+const comm1116= {
+    code: 'COMM 1116',
+    name: 'Business Communications 1'
+}
+
+const math1310 = {
+    code: 'MATH 1310',
+    name: 'Technical Math for IT'
+}
+
+
+let courseList = [acit1620,acit1515,acit1630,acit1420,orgb1100,comm1116,math1310]
+
